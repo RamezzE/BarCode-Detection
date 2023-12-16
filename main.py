066b62023ef7babf052cd4d9e809a9e4787be185
Ramez Ehab <ramezehab2@gmail.com>
2023-12-16 12:11:01 +0200

    Finalizing the methodology

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import cv2
 import os
 import imutils
 
-def rescaleFrame(frame, scale=0.5):
+showProcess = False
+
+
+def rescale_frame(frame, scale=0.5):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
 
 def calculate_brightness(image):
     # Convert the image to grayscale
@@ -18,71 +22,36 @@ def calculate_brightness(image):
 
     return brightness
 
-def find_parallel(lines):
-    max_parallel_count = 0
-    best_line_index = -1
-    best_parallel_indices = []
 
-    for i in range(len(lines)):
-        parallel_indices = []
-
-        for j in range(len(lines)):
-            if i == j:
-                continue
-
-            # Extract line coordinates
-            x1_i, y1_i, x2_i, y2_i = lines[i][0]
-            x1_j, y1_j, x2_j, y2_j = lines[j][0]
-
-            # Check if the lines are approximately parallel
-            delta_x_i = x2_i - x1_i
-            delta_y_i = y2_i - y1_i
-            delta_x_j = x2_j - x1_j
-            delta_y_j = y2_j - y1_j
-
-            # Avoid division by zero
-            if delta_x_i == 0 or delta_x_j == 0:
-                continue
-
-            slope_i = delta_y_i / delta_x_i
-            slope_j = delta_y_j / delta_x_j
-
-            if abs(slope_i - slope_j) < 0.05:
-                parallel_indices.append(j)
-
-        if len(parallel_indices) > max_parallel_count:
-            max_parallel_count = len(parallel_indices)
-            best_line_index = i
-            best_parallel_indices = parallel_indices
-
-    return best_line_index, best_parallel_indices
-
-def detectBarcodes2(image, contour):
-    showProcess = True
-
+# Refine & Validate Barcode Regions
+def detect_barcodes2(image, contour):
+    # Draw the contour on a mask
     mask = np.zeros(image.shape, dtype=np.uint8)
     cv2.drawContours(mask, [contour], -1, (255, 255, 255), -1)
 
     masked_image = cv2.bitwise_and(image, mask)
-    cv2.imshow("Masked Image", masked_image)
+
+    if showProcess:
+        cv2.imshow("Masked Image", masked_image)
 
     gray = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
 
-    # blurred = cv2.medianBlur(gray, 7)
     blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    edgeDetection = cv2.Canny(blurred, 50, 225, apertureSize=3)
+    # Canny Edge Detection
+    edge_detection = cv2.Canny(blurred, 50, 225, apertureSize=3)
 
     if showProcess:
-        cv2.imshow("Edge", edgeDetection)
+        cv2.imshow("Edge", edge_detection)
         cv2.waitKey(0)
 
-    lines = cv2.HoughLinesP(edgeDetection, rho=1, theta=np.pi / 180, threshold=25, minLineLength=100, maxLineGap=10)
+    # Hough Line Transform - Detect lines
+    lines = cv2.HoughLinesP(edge_detection, rho=1.25, theta=np.pi / 150, threshold=25, minLineLength=100, maxLineGap=10)
 
     blank = np.zeros(image.shape, dtype=np.uint8)
 
-    if lines is None:
-        print("No lines")
+    if lines is None or len(lines) < 2:
+        print("No lines or < 2")
         return False
 
     for i in range(len(lines)):
@@ -98,18 +67,14 @@ def detectBarcodes2(image, contour):
 
     red_pixels_x = np.where(blank[:, :, 2] == 255)[1]
 
-    # Find the minimum and maximum x-coordinates
     x_min = np.min(red_pixels_x)
     x_max = np.max(red_pixels_x)
 
-    # Find the y-coordinates where red channel is 255
     red_pixels_y = np.where(blank[:, :, 2] == 255)[0]
 
-    # Find the minimum and maximum y-coordinates
     y_min = np.min(red_pixels_y)
     y_max = np.max(red_pixels_y)
 
-    # Print the results
     print("Min X:", x_min)
     print("Max X:", x_max)
     print("Min Y:", y_min)
@@ -132,35 +97,39 @@ def detectBarcodes2(image, contour):
 
         point[0] = [x, y]
 
-    # Draw the contour on the image
     return cnt
 
-def detectBarcodes(image, showProcess=False):
+
+# Detect Barcode Regions
+def detect_barcodes(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    blurred = cv2.medianBlur(gray, 3)
+    # blurred = cv2.medianBlur(gray, 3)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    edgeDetection = cv2.Canny(blurred, 50, 225)
-    edgeDetection = cv2.convertScaleAbs(edgeDetection)
+    # Canny Edge Detection
+    edge_detection = cv2.Canny(blurred, 50, 225)
+    edge_detection = cv2.convertScaleAbs(edge_detection)
 
     if showProcess:
-        cv2.imshow("1 - Edge", edgeDetection)
+        cv2.imshow("1 - Edge", edge_detection)
 
     kernel = np.ones((7, 7), np.uint8)
 
-    # Calculate the mean brightness of the image
     brightness = calculate_brightness(image)
 
-    if brightness > 170:  # Adjust thresholds for bright images
-        erode_dilate = edgeDetection
+    # Skip dilation and erosion if image is bright
+    if brightness > 170:
+        erode_dilate = edge_detection
     else:
-        erode_dilate = cv2.dilate(edgeDetection, kernel, iterations=1)
+        erode_dilate = cv2.dilate(edge_detection, kernel, iterations=1)
         if showProcess:
             cv2.imshow("2 - Dilate", erode_dilate)
         erode_dilate = cv2.erode(erode_dilate, np.ones((11, 11), np.uint8), iterations=1)
         if showProcess:
             cv2.imshow("3 - Erode", erode_dilate)
 
+    # Morphological Closing
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
     closed = cv2.morphologyEx(erode_dilate, cv2.MORPH_CLOSE, kernel)
 
@@ -171,21 +140,25 @@ def detectBarcodes(image, showProcess=False):
         closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
-    largestContourArea = cv2.contourArea(
+    if len(cnts) == 0:
+        print("No contours found")
+        return
+
+    largest_contour_area = cv2.contourArea(
         sorted(cnts, key=cv2.contourArea, reverse=True)[0])
 
     for cnt in cnts:
         # Filter contours based on area
-        if cv2.contourArea(cnt) < (largestContourArea / 2):
+        if cv2.contourArea(cnt) < (largest_contour_area / 2):
             continue
 
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.intp(box)
 
-        cnt_original = box.copy()
+        box_original = box.copy()
 
-        cnt = detectBarcodes2(image, cnt)
+        cnt = detect_barcodes2(image, cnt)
 
         if cnt is False:
             continue
@@ -194,9 +167,8 @@ def detectBarcodes(image, showProcess=False):
         box = cv2.boxPoints(rect)
         box = np.intp(box)
 
-        # Draw a bounding box around each detected region
         cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
-        cv2.drawContours(image, [cnt_original], -1, (255, 0, 0), 1)
+        # cv2.drawContours(image, [box_original], -1, (255, 0, 0), 3)
 
     image_name = os.path.basename(image_path)
     cv2.imshow(f"Detected Barcode {image_name}", image)
@@ -204,21 +176,20 @@ def detectBarcodes(image, showProcess=False):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 folderPath = 'Images'
 
 # List all files in the folder
 files = os.listdir(folderPath)
 
 for file in files:
-    # Check if the file is an image
     if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-        # Construct the full path to the image
+
         image_path = os.path.join(folderPath, file)
 
-        # Read the image
         image = cv2.imread(image_path)
 
         if image.shape[0] > 800 or image.shape[1] > 1500:
-            image = rescaleFrame(image)
+            image = rescale_frame(image)
 
-        detectBarcodes(image, False)
+        detect_barcodes(image)
